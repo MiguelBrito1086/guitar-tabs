6324c390514a281cd4274a470428ea517881edbe
MiguelBrito1086 <mbrito1086@gmail.com>
2018-05-25 15:07:32 -0400

    added a call to server with authenticationService

---
+++ project/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="Email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="Password" v-model="password">
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService';
+export default {
+  data () {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    async register() {
+      await authenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log("Response: ", response.data);
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
---
+++ project/src/services/authenticationService.js
@@ -0,0 +1,14 @@
+import api from '@/services/api';
+
+export default {
+  register (credentials) {
+    return api().post('register', credentials)
+  }
+}
+
+
+
+// authenticationService.register({
+//   email: 'testing@gmail.com',
+//   password: '12345'
+// })
